6507a2e6705cee87d5bc1821fac7a16bf92faf7c
andresbotia <abotia2018@fau.edu>
2021-06-23 13:39:49 -0400

    adding table + changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
 import React, {useState} from "react";
 
 function Home() {
-  const [number1, setNumber1] = useState();
-  const [number2, setNumber2] = useState(1.5);
-  const [total, setTotal] = useState(number1 - number2);
-  function calculateTotal() {
-    setTotal(number1 - number2);
-    // setTotal2(number1 - number3);
-    // setTotal3(number1 + number4);
+  // const [number1, setNumber1] = useState();
+  // const [number2, setNumber2] = useState(1.5);
+  // const [number3, setNumber3] = useState(2);
+  // const [total, setTotal] = useState(number1 - number2);
+  // const [total2, setTotal2] = useState(number1 - number3);
+  // const [total3, setTotal3] = useState(number1 + number4);
+  // function calculateTotal() {
+  //   setTotal(number1 - number2);
+  //   setTotal2(number1 - number3);
+  //   setTotal3(number1 + number4);
   }
   return (
     <div className="home">
@@ -16,20 +19,20 @@ function Home() {
         
           <div class="col-lg-7">
           <h1 class="font-weight-light">Enter Shoe Size</h1>
-          <input
+          {/* <input
             type="number"
             value={number1}
             onChange={e => setNumber1(+e.target.value)}
             placeholder="0"
-          />
-          <div>
+          /> */}
+          {/* <div>
           <button onClick={calculateTotal}>Click to Calculate</button>
-          </div>
+          </div> */}
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">You can also shop in these sizes:</h1>
             
-            <table style={{border: "1px solid black"}}>
+            {/* <table style={{border: "1px solid black"}}>
               <tr>
                 <th>Men :</th>
                 <th>{total}</th>
@@ -37,20 +40,20 @@ function Home() {
               </tr>
               <tr>
                 <th>Kids :</th>
-                <th>{total}</th>
+                <th>{total2}</th>
                 <th></th>
               </tr>
               <tr>
                 <th>EU :</th>
-                <th>{total}</th>
+                <th>{total3}</th>
                 <th></th>
               </tr>
               <tr>
                 <th>UK :</th>
-                <th>{total}</th>
+                <th>{total2}</th>
                 <th></th>
               </tr>
-            </table>
+            </table> */}
           </div>
         </div>
       </div>
